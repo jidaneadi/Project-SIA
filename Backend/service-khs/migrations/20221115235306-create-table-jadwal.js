5327f96d2c42dd81e7bf4da0dc09241e620a5c08
unknown <jidaneadir11@gmail.com>
2022-11-21 14:30:14 +0700

    Update migration in service-khs v2

--- Backend/service-khs/migrations/20221115235306-create-table-jadwal.js
+++ Backend/service-khs/migrations/20221115235306-create-table-jadwal.js
@@ -1,12 +1,38 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    
+    await queryInterface.createTable('jadwal', {
+      id : {
+        type : Sequelize.INTEGER,
+        primaryKey : true,
+        allowNull : false,
+        autoIncrement : true
+      },
+      id_mapel : {
+        type : Sequelize.INTEGER,
+        allowNull : false
+      },
+      id_guru : {
+        type : Sequelize.INTEGER,
+        allowNull : false
+      },
+      id_ruang : {
+        type : Sequelize.INTEGER,
+        allowNull : false
+      },
+      id_kelas : {
+        type : Sequelize.INTEGER,
+        allowNull : false
+      },
+      hari_waktu : {
+        type : Sequelize.STRING,
+        allowNull : true
+      }
+    })
   },
 
   async down (queryInterface, Sequelize) {
-    
+    await queryInterface.dropTable('jadwal');
   }
 };
